cf6d27b148d913be2f9de494d2f6f4a5418686ef
Finleyw <511346@chrome.escg.ac.uk>
2021-03-25 13:43:18 +0000

    ye

---
+++ Assets/Scripts/General.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class General : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void TestButtonSCript( string myMessage )
+    {
+        Debug.Log( myMessage );
+    }
+
+    public void LoadScene(string level)
+    { 
+        SceneManager.LoadScene(level);
+    }
+
+    public void quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+    
+}
