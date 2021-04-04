5a0f9864aa3d778a571aa744c9947070bf1d17d4
Finleyw <511346@chrome.escg.ac.uk>
2021-04-04 14:54:04 +0100

    wampy

--- Assets/Scripts/General.cs
+++ Assets/Scripts/General.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 using UnityEngine;
 
 public class General : MonoBehaviour
 {
+    public GameObject menuFirst, optionsFirst, difficultyFirst, loadFirst,audioFirst,h2pFirst;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +18,37 @@ public class General : MonoBehaviour
     {
         
     }
-
+    public void MenuNav()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(menuFirst);
+    }
+    
+    public void OptionsNav()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(optionsFirst);
+    }
+    public void DifficultyNav()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(difficultyFirst);
+    }
+    public void LoadNav()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(loadFirst);
+    }
+    public void AudioNav()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(audioFirst);
+    }
+    public void H2pNav()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(h2pFirst);
+    }
 
     public void TestButtonSCript( string myMessage )
     {
